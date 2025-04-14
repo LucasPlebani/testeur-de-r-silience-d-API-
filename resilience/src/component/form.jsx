8bdbc44147e08c616e3d06e5a2d833766d7d87cc
Lucas Plebani <lucas.plebani@outlook.fr>
2025-04-14 11:27:26 +0200

    create components form and css

---
+++ resilience/src/component/form.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style/form.css";
+function Form() {
+  return (
+    <div className="form">
+      <h1>Form</h1>
+      <p>ceci est le formulaire</p>
+    </div>
+  );
+}
+export default Form;
