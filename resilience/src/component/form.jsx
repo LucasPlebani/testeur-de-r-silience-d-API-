9d2b7eadac5ed850b63ff7333eb8979ad228f905
Lucas Plebani <lucas.plebani@outlook.fr>
2025-04-14 12:00:37 +0200

    add form and template button

--- resilience/src/component/form.jsx
+++ resilience/src/component/form.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import "./style/form.css";
+import Button from "./button.jsx";
 function Form() {
-  return (
-    <div className="form">
-      <h1>Form</h1>
-      <p>ceci est le formulaire</p>
-    </div>
-  );
+    const [apiUrl, setApiUrl] = React.useState("");
+    const [requestCount, setRequestCount] = React.useState(1);
+
+    const handleApiUrlChange = (e) => setApiUrl(e.target.value);
+    const handleRequestCountChange = (e) => setRequestCount(e.target.value);
+
+    return (
+        <div className="form">
+            <h1>Form</h1>
+            <p>Ce formulaire permet de configurer l'API cible</p>
+            <div>
+                <label htmlFor="apiUrl">URL de l'API cible:</label>
+                <input
+                    type="text"
+                    id="apiUrl"
+                    value={apiUrl}
+                    onChange={handleApiUrlChange}
+                    placeholder="Entrez l'URL de l'API"
+                />
+            </div>
+            <div>
+                <label htmlFor="requestCount">Nombre de requêtes à envoyer:</label>
+                <input
+                    type="number"
+                    id="requestCount"
+                    value={requestCount}
+                    onChange={handleRequestCountChange}
+                    min="1"
+                />
+                <Button />
+            </div>
+        </div>
+    );
 }
 export default Form;
